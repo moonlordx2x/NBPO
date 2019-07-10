1a9abc694895853b2ad2d071f25f5dbe8370faa3
moonlordx2x <moonlordx2x>
2019-07-10 23:27:46 +0800

    sample

---
+++ app/src/main/java/com/example/neilbryanlagrimas/nbpo_gameapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.neilbryanlagrimas.nbpo_gameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView userText;
+    Button btnSave;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference();
+
+        userText = findViewById(R.id.Name);
+        btnSave = findViewById(R.id.Save);
+    }
+}
