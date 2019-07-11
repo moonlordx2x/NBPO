006a2ec0927d957414d24b06cd80353a4624c41e
kitsunex2x <moonlordx2x@gmail.com>
2019-07-11 15:51:59 +0800

    Updated

---
+++ app/src/main/java/com/example/neilbryanlagrimas/nbpo_gameapp/Member.java
@@ -0,0 +1,26 @@
+package com.example.neilbryanlagrimas.nbpo_gameapp;
+
+public class Member {
+
+    private String Name;
+    private String LName;
+
+    public Member() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLName() {
+        return LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+}
