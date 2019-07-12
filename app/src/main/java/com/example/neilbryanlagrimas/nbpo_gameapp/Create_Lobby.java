380f11d1e797fe0ee682ff15d3217ba2b50efd43
kitsunex2x <moonlordx2x@gmail.com>
2019-07-12 16:43:11 +0800

    Ongoing Game App Development

---
+++ app/src/main/java/com/example/neilbryanlagrimas/nbpo_gameapp/Create_Lobby.java
@@ -0,0 +1,18 @@
+package com.example.neilbryanlagrimas.nbpo_gameapp;
+
+public class Create_Lobby {
+
+    private String Lobby_name;
+
+
+    public Create_Lobby() {
+    }
+
+    public String getLobby_name() {
+        return Lobby_name;
+    }
+
+    public void setLobby_name(String lobby_name) {
+        Lobby_name = lobby_name;
+    }
+}
